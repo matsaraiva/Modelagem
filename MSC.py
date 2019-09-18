9f36b8ef3df7df0ed70608cce01192b217aff12f
matsaraiva <52610357+matsaraiva@users.noreply.github.com>
2019-09-18 12:41:51 -0300

    Update MSC.py

--- MSC.py
+++ MSC.py
@@ -1,20 +1,34 @@
 import random
 import os
 
+matriz = []
+
 def main():
     print("hello")
     print(gerar_matriz(2, 2))
+    gerar_grupo_de_animais()
 
 def gerar_matriz (n_linhas, n_colunas):
-    matriz = []
 
-    for _ in range(n_linhas):
+    for i in range(n_linhas):
         matriz.append( [0] * n_colunas )
 
     return matriz
     
-def gerar_animais():
-    x = input("x")
-    y = input("y")
+def gerar_grupo_de_animais():
+    x = input("x1")
+    y = input("y1")
+
+    matriz[0][0] = x
+    matriz[0][1] = y
+
+    x = input("x2")
+    y = input("y2")
+
+    matriz[1][0] = x
+    matriz[1][1] = y
+
+    print(matriz)
+
 
 main()
