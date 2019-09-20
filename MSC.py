4921e4979b8b734242b638ca505f119af54fa911
matsaraiva <52610357+matsaraiva@users.noreply.github.com>
2019-09-20 16:09:12 -0300

    Update MSC.py

--- MSC.py
+++ MSC.py
@@ -5,30 +5,62 @@ matriz = []
 
 def main():
     print("hello")
-    print(gerar_matriz(2, 2))
-    gerar_grupo_de_animais()
+    print(gerar_mapa(4, 4))
+    gerar_paredes()
+    snake(1, 1)
+    prey(2, 2)
+    move()
 
-def gerar_matriz (n_linhas, n_colunas):
+def gerar_mapa (n_linhas, n_colunas):
 
     for i in range(n_linhas):
         matriz.append( [0] * n_colunas )
 
     return matriz
+
+def snake(x, y):
+    matriz[x][y] = 2
     
-def gerar_grupo_de_animais():
-    x = input("x1")
-    y = input("y1")
+    print(matriz)
 
-    matriz[0][0] = x
-    matriz[0][1] = y
+def prey(x, y):
+    matriz[x][y] = 3
+    
+    print(matriz)
 
-    x = input("x2")
-    y = input("y2")
+def gerar_paredes():
 
-    matriz[1][0] = x
-    matriz[1][1] = y
+    matriz[0][0] = 1
+    matriz[0][1] = 1
+    matriz[0][2] = 1
+    matriz[0][3] = 1
+
+    matriz[1][0] = 1
+    matriz[1][3] = 1
+
+    matriz[2][0] = 1
+    matriz[2][3] = 1
+
+    matriz[3][0] = 1
+    matriz[3][1] = 1
+    matriz[3][2] = 1
+    matriz[3][3] = 1
 
     print(matriz)
 
+def move():
+    r = random.randrange(0, 3)
+    
+    if r == 1:
+        matriz[1][1] = 0
+        matriz[1][0] = 2
+    elif r == 2:
+        matriz[1][1] = 0
+        matriz[1][2] = 2
+    else
+        matriz[1][1] = 0
+        matriz[0][1] = 2
+
+    print(matriz)
 
 main()
