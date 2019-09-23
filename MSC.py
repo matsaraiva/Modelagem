2e49976c1ab8260bb6d74d4af6dca6264d9e0482
matsaraiva <52610357+matsaraiva@users.noreply.github.com>
2019-09-23 18:54:56 -0300

    Update MSC.py

--- MSC.py
+++ MSC.py
@@ -2,13 +2,18 @@ import random
 import os
 
 matriz = []
+direcao = (random.randrange(0, 3)) #0:norte 1:leste 2:sul 3:oeste
+print("direcao: ", direcao)
+
+pi = 1
+pj = 1
 
 def main():
     gerar_mapa(4, 4)
-    gerar_paredes()
+    gerar_paredes(4, 4)
     snake(1, 1)
     prey(2, 2)
-    move()
+    move(pi, pj)
     mostrar(4, 4)
 
 def gerar_mapa (n_linhas, n_colunas):
@@ -28,38 +33,61 @@ def prey(x, y):
     
     mostrar(4, 4)
 
-def gerar_paredes():
+def gerar_paredes(n_linhas, n_colunas):
 
-    matriz[0][0] = 1
-    matriz[0][1] = 1
-    matriz[0][2] = 1
-    matriz[0][3] = 1
+    for i in range (n_colunas):
+        matriz[0][i] = 1                #parede superior
+        matriz[n_linhas - 1][i] = 1         #parede inferior
 
-    matriz[1][0] = 1
-    matriz[1][3] = 1
-
-    matriz[2][0] = 1
-    matriz[2][3] = 1
-
-    matriz[3][0] = 1
-    matriz[3][1] = 1
-    matriz[3][2] = 1
-    matriz[3][3] = 1
+    for i in range (n_linhas - 2):
+        matriz[i + 1][0] = 1            #parede esquerda
+        matriz[i + 1][n_colunas - 1] = 1    #parede direita
 
     mostrar(4, 4)
 
-def move():
-    r = random.randrange(0, 3)
+def move(pi,pj):
+    r = random.randrange(0, 2)
+
+    matriz[pi][pj] = 0
     
     if r == 1:
-        matriz[1][1] = 0
-        matriz[1][0] = 2
+
+        if direcao == 0:
+            pj += -1
+        elif direcao == 1:
+            pi += -1
+        elif direcao == 2:
+            pj += 1
+        else:
+            pi += 1
+
+        matriz[pi][pj] = 2
+
     elif r == 2:
-        matriz[1][1] = 0
-        matriz[1][2] = 2
+
+        if direcao == 0:
+            pj += 1
+        elif direcao == 1:
+            pi += 1
+        elif direcao == 2:
+            pj += -1
+        else:
+            pi += -1
+
+        matriz[pi][pj] = 2
+
     else:
-        matriz[1][1] = 0
-        matriz[0][1] = 2
+
+        if direcao == 0:
+            pi += -1
+        elif direcao == 1:
+            pj += 1
+        elif direcao == 2:
+            pi += 1
+        else:
+            pj += -1
+
+        matriz[pi][pj] = 2
 
     mostrar(4, 4)
 
