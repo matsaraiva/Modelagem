19d90bcddae90852bf8a8b2c1cc984849377a0a3
matsaraiva <52610357+matsaraiva@users.noreply.github.com>
2019-09-23 20:18:42 -0300

    Update MSC.py

--- MSC.py
+++ MSC.py
@@ -3,18 +3,20 @@ import os
 
 matriz = []
 direcao = (random.randrange(0, 3)) #0:norte 1:leste 2:sul 3:oeste
-print("direcao: ", direcao)
 
-pi = 1
-pj = 1
 
 def main():
-    gerar_mapa(4, 4)
-    gerar_paredes(4, 4)
-    snake(1, 1)
-    prey(2, 2)
-    move(pi, pj)
-    mostrar(4, 4)
+    pi = random.randrange(2, 13)
+    pj = random.randrange(2, 19)
+    live = 1
+
+    gerar_mapa(15, 21)
+    gerar_paredes(15, 21)
+    snake(pi, pj)
+    prey(random.randrange(2, 13), random.randrange(2, 19))
+    while live == 1:
+        pi, pj, live = move(pi, pj, live)
+    defeat()
 
 def gerar_mapa (n_linhas, n_colunas):
 
@@ -26,70 +28,70 @@ def gerar_mapa (n_linhas, n_colunas):
 def snake(x, y):
     matriz[x][y] = 2
     
-    mostrar(4, 4)
+    mostrar(15, 21)
 
 def prey(x, y):
     matriz[x][y] = 3
     
-    mostrar(4, 4)
+    mostrar(15, 21)
 
 def gerar_paredes(n_linhas, n_colunas):
 
     for i in range (n_colunas):
-        matriz[0][i] = 1                #parede superior
+        matriz[0][i] = 1                    #parede superior
         matriz[n_linhas - 1][i] = 1         #parede inferior
 
     for i in range (n_linhas - 2):
-        matriz[i + 1][0] = 1            #parede esquerda
+        matriz[i + 1][0] = 1                #parede esquerda
         matriz[i + 1][n_colunas - 1] = 1    #parede direita
 
-    mostrar(4, 4)
+    mostrar(15, 21)
 
-def move(pi,pj):
+def move(pit,pjt, live):
     r = random.randrange(0, 2)
 
-    matriz[pi][pj] = 0
+    matriz[pit][pjt] = 0
     
     if r == 1:
 
         if direcao == 0:
-            pj += -1
+            pjt += -1
         elif direcao == 1:
-            pi += -1
+            pit += -1
         elif direcao == 2:
-            pj += 1
+            pjt += 1
         else:
-            pi += 1
-
-        matriz[pi][pj] = 2
+            pit += 1
 
     elif r == 2:
 
         if direcao == 0:
-            pj += 1
+            pjt += 1
         elif direcao == 1:
-            pi += 1
+            pit += 1
         elif direcao == 2:
-            pj += -1
+            pjt += -1
         else:
-            pi += -1
-
-        matriz[pi][pj] = 2
+            pit += -1
 
     else:
 
         if direcao == 0:
-            pi += -1
+            pit += -1
         elif direcao == 1:
-            pj += 1
+            pjt += 1
         elif direcao == 2:
-            pi += 1
+            pit += 1
         else:
-            pj += -1
+            pjt += -1
 
-        matriz[pi][pj] = 2
+    if matriz[pit][pjt] == 1:
+        live = 0
 
-    mostrar(4, 4)
+    matriz[pit][pjt] = 2
+    mostrar(15, 21)
+
+    return pit, pjt, live
 
 def mostrar(linhas, colunas):
 
@@ -100,6 +102,10 @@ def mostrar(linhas, colunas):
   
     input("\nAperte enter para continuar")
     clear()
+
+def defeat():
+    print(" derrota")
+
 
 def clear():
     os.system('cls' if os.name=='nt' else 'clear')
