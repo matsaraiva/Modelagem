1bd8f7aaba70aa8a2b32fe032bb1f93646c23f84
matsaraiva <52610357+matsaraiva@users.noreply.github.com>
2019-11-28 17:39:44 -0300

    Update environment.py

--- environment.py
+++ environment.py
@@ -1,23 +1,27 @@
 import time
 import numpy
+import random
 
 class Env():
     def __init__(self):
         self.altura = 5
-        self.largura = 9
-        self.posX = 1
-        self.posY = self.altura-2
-        self.alvoX = self.largura-2
-        self.alvoY = self.altura-2
+        self.largura = 16
+        self.posX = 5
+        self.posY = 2
+        self.alvoX = 0
+        self.alvoY = random.randrange(1, self.altura-2)
+        self.alvobonusX = self.largura-1
+        self.alvobonusY = random.randrange(1, self.altura-2)
         self.escolhas = [0, 1, 2, 3]
         self.posicao_atualQtd = self.altura*self.largura
         self.escolhaQtd = len(self.escolhas)
 
     def reset(self):
-        self.posX = 1;
-        self.posY = self.altura-2;
+        self.posX = 5;
+        self.posY = 2;
         self.fim = False;
-        return 0, 0, False;
+        self.fim_melhor = False
+        return 37, 0, False, False;
 
     # take escolha
     def mover(self, escolha):
@@ -30,8 +34,10 @@ class Env():
         if escolha==3: # down
             self.posY = self.posY+1 if self.posY<self.altura-1 else self.posY;
 
-        fim = self.posX==self.alvoX and self.posY==self.alvoY;
-
+        
+        
+        fim = (self.posX==self.alvoX and self.posY==self.alvoY)
+        fim_melhor = (self.posX==self.alvobonusX and self.posY==self.alvobonusY)
 
         # proxima posição da IA, baseado no mapeamento de X e Y para cada posição na grade
         proxima_posicao = self.largura*self.posY + self.posX;
@@ -40,7 +46,8 @@ class Env():
         #time.sleep(0.1)
 
         recompensa = 1 if fim else 0;
-        return proxima_posicao, recompensa, fim;
+        recompensa = 100 if fim_melhor else 0;
+        return proxima_posicao, recompensa, fim, fim_melhor;
 
     # return a random escolha
     def randomescolha(self):
@@ -50,9 +57,11 @@ class Env():
         for i in range(self.altura):
             for j in range(self.largura):
                 if self.posY==i and self.posX==j:
-                    print("1 ", end='');
+                    print("Q ", end='');
                 elif self.alvoY==i and self.alvoX==j:
-                    print("2 ", end='');
+                    print("A ", end='');
+                elif self.alvobonusY==i and self.alvobonusX==j:
+                    print("B ", end='');
                 else:
-                    print("0 ", end='');
+                    print("_ ", end='');
             print("");
